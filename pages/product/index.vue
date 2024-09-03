f67256aa1e5defde14c3594efa0158a8febece9b
AomSirawit <110830004+AomSirawit@users.noreply.github.com>
2024-09-03 09:09:08 +0700

    delete more button in product

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -49,13 +49,6 @@
             </div>
           </div>
         </div>
-        <div class="flex justify-center items-center">
-          <NuxtLink
-            to="/product"
-            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-            More
-          </NuxtLink>
-        </div>
       </div>
      
     </div>
